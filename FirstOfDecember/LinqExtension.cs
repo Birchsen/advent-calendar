37078fd74a53363bac0263671211fbe32549cf9a
Jonathan Birch Jensen <Jonathanbjensen@gmail.com>
2021-12-04 14:09:39 +0100

    Added solution for first of december

---
+++ FirstOfDecember/LinqExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace FirstOfDecember;
+public static class LinqExtension
+{
+    public static int CountMeasurements(this IEnumerable<int> source)
+    {
+        int measurements = 0;
+        int previousMeasurement = int.MaxValue;
+        foreach (var item in source)
+        {
+            if (item > previousMeasurement)
+            {
+                measurements++;
+            }
+            previousMeasurement = item;
+        }
+
+        return measurements;
+    }
+}
---
+++ FirstOfDecember/Program.cs
@@ -0,0 +1,13 @@
+﻿using FirstOfDecember;
+
+string path = @"D:\C#\Julekalender\Julekalender\FirstOfDecember\measurements.txt";
+
+if (File.Exists(path))
+{
+    var measurements = File.ReadLines(path)
+        .Where(m => !string.IsNullOrWhiteSpace(m))
+        .Select(m => int.Parse(m))
+        .CountMeasurements();
+
+    Console.WriteLine(measurements);
+}
