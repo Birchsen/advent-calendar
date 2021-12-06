8a4325775d2705e71fd7cbe96b86396336577fa7
Jonathan Birch Jensen <Jonathanbjensen@gmail.com>
2021-12-07 00:38:18 +0100

    Refined first of december to use LINQ built-ins

--- FirstOfDecember/Program.cs
+++ FirstOfDecember/Program.cs
@@ -1,13 +1,28 @@
-﻿using FirstOfDecember;
-
-string path = @"D:\C#\Julekalender\Julekalender\FirstOfDecember\measurements.txt";
+﻿string path = @"D:\C#\Julekalender\Julekalender\FirstOfDecember\measurements.txt";
 
 if (File.Exists(path))
 {
-    var measurements = File.ReadLines(path)
-        .Where(m => !string.IsNullOrWhiteSpace(m))
-        .Select(m => int.Parse(m))
-        .CountMeasurements();
+    var lines = File.ReadAllLines(path);
 
-    Console.WriteLine(measurements);
+    var numbers = lines.Select(x => int.Parse(x));
+
+    var measurements = Enumerable
+        .Zip(numbers, numbers.Skip(1))
+        .Where(x => x.First < x.Second)
+        .Count();
+
+    var numbersThreeMeasurements = Enumerable
+        .Zip(numbers, numbers.Skip(1), numbers.Skip(2))
+        .Select(x => x.First + x.Second + x.Third);
+
+    var threeMeasurements = Enumerable
+        .Zip(numbersThreeMeasurements, numbersThreeMeasurements.Skip(1))
+        .Where(x => x.First < x.Second)
+        .Count();
+
+    var outputString = $@"Day 1 - Advent of Code 2021
+Part 1 - How many measurements are larger than the previous measurement? Answer: {measurements}
+Part 2 - How many measurements are larger than the previous sum of measurements? Answer: {threeMeasurements}";
+
+    Console.WriteLine(outputString);
 }
